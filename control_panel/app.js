01a5e72d313e84434fd4023ef4c210a46166f8df
AseelArafeh <arafehaseel@gmail.com>
2020-03-14 15:27:36 +0200

    preparing routes file

--- control_panel/app.js
+++ control_panel/app.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+const express = require("express")
+const app = express()
+
+app.get('/', (req,res)=> {
+    res.send("Hello World!")
+})
+
+// bringg events routes
+const events = require('./routes/event-routes')
+app.use('/events', events)
+
+// listen to port 3000
+app.listen(3000, ()=> {
+    console.log(' app is wokring on port 3000')
+})
