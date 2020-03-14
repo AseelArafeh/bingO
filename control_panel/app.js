455681117edf9b5607f3bd644593ecd45fd04574
AseelArafeh <arafehaseel@gmail.com>
2020-03-14 23:24:01 +0200

    Added routing for all pages

--- control_panel/app.js
+++ control_panel/app.js
@@ -1,13 +1,45 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const hbs = require('express-handlebars');
+
+app.engine("hbs", hbs({
+    extname: "hbs",
+    defaultLayout: "mainlayout",
+    layoutDir: __dirname + "/views/layouts/"
+}));
+app.set('view engine', 'hbs');
+ 
 
 app.get('/', (req,res)=> {
-    res.send("Hello World!")
-})
+    res.render('pages/home');
+});
+app.get('/home', (req,res)=> {
+    res.render('pages/home');
+});
+app.get('/about', (req, res) => {
+    res.render('pages/about');
+});
+app.get('/categories', (req,res)=> {
+    res.render('pages/categories');
+});
+app.get('/items', (req,res)=> {
+    res.render('pages/items');
+});
+app.get('/login', (req,res)=> {
+    res.render('pages/login');
+});
+app.get('/profile', (req,res)=> {
+    res.render('pages/profile');
+});
+app.get('/users', (req,res)=> {
+    res.render('pages/users');
+});
 
+/*
 // bringg events routes
 const events = require('./routes/event-routes')
-app.use('/events', events)
+app.use('/events', events) 
+ */
 
 // listen to port 3000
 app.listen(3000, ()=> {
