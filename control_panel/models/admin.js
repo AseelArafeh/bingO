a892c114b398433a8e2ca71391912d0e005f2d34
AseelArafeh <arafehaseel@gmail.com>
2020-03-15 01:29:23 +0200

    added admin schema

---
+++ control_panel/models/admin.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const adminSchema = new mongoose.Schema({
+    adminName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+let admin = mongoose.model('admin', adminSchema, 'aboutsCollection')
+
+module.exports = admin
