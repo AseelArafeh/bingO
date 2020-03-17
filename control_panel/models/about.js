76bd1d2aad2b8eaf6a30f491f187da977a0e4d99
Qusai Hroub <qusaihroub.r@gmail.com>
2020-03-17 11:33:16 +0200

    Add edit functionality to model of about

--- control_panel/models/about.js
+++ control_panel/models/about.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
 const aboutSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -12,8 +13,47 @@ const aboutSchema = new mongoose.Schema({
         type: Date,
         required: true
     }
-})
+});
+
+aboutSchema.methods.updateAboutTitle = (self, newTitle) => {
+    self.title = newTitle;
+    self.save(function(err, about) {
+        if (err) {
+            console.error(err);
+            return 1;
+        }
+    })
+    return 0;
+}
+
+itemaboutSchema.methods.updateAboutDescription = (self, newDescription) => {
+    self.description = newDescription;
+    self.save(function(err, about) {
+        if (err) {
+            console.error(err);
+            return 1;
+        }
+    })
+    return 0;
+}
+
+aboutSchema.methods.updateAboutDate = (self, newDate) => {
+    self.date = newDate;
+    self.save(function(err, about) {
+        if (err) {
+            console.error(err);
+            return 1;
+        }
+    })
+    return 0;
+}
+
+aboutSchema.methods.updateAbout = (self, newTitle, newDescription, newDate) => {
+    self.updateAboutTitle(self, newTitle);
+    self.updateAboutDescription(self, newDescription);
+    self.updateAboutDate(self, newDate);
+}
 
 let about = mongoose.model('about', aboutSchema, 'aboutsCollection')
 
-module.exports = about
+module.exports = about;
