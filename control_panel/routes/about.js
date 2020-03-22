36bba657c174cffbbae1016acdc9876d39641cd4
Qusai Hroub <qusaihroub.r@gmail.com>
2020-03-22 14:28:18 +0200

    Add middleware to check if admin is logged in

--- control_panel/routes/about.js
+++ control_panel/routes/about.js
@@ -4,7 +4,14 @@ let router = express.Router();
 let mongoose = require('./../config/database');
 let About = require('./../models/about');
 
-router.post('/about/update', (req, res, next) => {
+// middleware to check if admin is logged in
+isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) return next()
+    res.redirect('/admin/login')
+}
+
+// save updates on about us infos.
+router.post('/about/update', isAuthenticated, (req, res, next) => {
 
     About.findById(req.body._id, (err, about) => {
         if (err) throw err;
